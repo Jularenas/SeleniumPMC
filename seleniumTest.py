33ff494c2b74b6ee1d2e79e4079d579c10fdc3f0
Julian Arenas <jd.arenas@uniandes.edu.co>
2018-04-03 13:19:08 -0500

    submit form electricistas 24 horas bogota

--- seleniumTest.py
+++ seleniumTest.py
@@ -13,9 +13,28 @@ chrome_driver = os.getcwd() +"\\chromedriver.exe"
 
 # go to Google and click the I'm Feeling Lucky button
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-driver.get("https://docs.google.com/forms/d/e/1FAIpQLSeSuAoKBxuiQNDs-M8YpDKJys9oqokRzyC-yQ0p710ig1UwcQ/viewform?fbzx=3067989463602548000")
+driver.get("http://www.electricistas24horasbogota.com/contactenos")
 #lucky_button = driver.find_element_by_css_selector("[name=btnI]")
-submit = driver.find_elements_by_class_name("quantumWizButtonPaperbuttonLabel exportLabel")
+nameField = driver.find_element_by_id("page93_inputsinglevalue1_inputsinglevalue1")
+nameField.clear()
+nameField.send_keys("Julian")
+
+phoneField = driver.find_element_by_id("page93_inputsinglevalue2_phone")
+phoneField.clear()
+phoneField.send_keys("3142352865")
+
+emailField = driver.find_element_by_id("page93_inputsinglevalue3_email")
+emailField.clear()
+emailField.send_keys("jularenas2@gmail.com")
+
+comentarioField = driver.find_element_by_id("page93_inputsinglevalue4_inputsinglevalue4")
+comentarioField.clear()
+comentarioField.send_keys("Reparacion cableado hogar")
+
+submit=driver.find_element_by_id("page93_formcontainer1_form")
+submit.submit()
+
+
 #lucky_button.click()
 print(driver.title)
 # capture the screen
