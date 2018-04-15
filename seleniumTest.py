a02116ee955b8c0624cdc05b2b405c3151ed2d14
Julian Arenas <jd.arenas@uniandes.edu.co>
2018-04-15 14:37:47 -0500

    doctorSolución1/2

--- seleniumTest.py
+++ seleniumTest.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 import os
 
 # instantiate a chrome options object so you can set the size and headless preference
@@ -106,5 +107,88 @@ def expertoYa(name,phone,email,desc,dir,now,date,time):
  print(driver.title)
  # capture the screen
  driver.get_screenshot_as_file("captureExpertoYa.png")
+ 
+ 
+# def doctorSolucion(name,phone,phone2,email,desc,departamento,ciudad,barrio,):
+ # driver.get("http://www.doctorsolucion.co/cita-presupuesto")
+ # #lucky_button = driver.find_element_by_css_selector("[name=btnI]")
+ # nameField = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_txtNome_I")
+ # nameField.clear()
+ # nameField.send_keys(name)
+ 
+ # emailField = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_txtEmail_I")
+ # emailField.clear()
+ # emailField.send_keys(email)
+
+ # phoneField = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_txtTelefoneCelular_I")
+ # phoneField.clear()
+ # phoneField.send_keys(phone)
+ 
+ # phoneField2 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_txtTelefone_I")
+ # phoneField2.clear()
+ # phoneField2.send_keys(phone2)
+ 
+ 
+ # direccionField = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_txtEndereco_I")
+ # direccionField.clear()
+ # direccionField.send_keys()
+ 
+ # selectDepartamento = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_cboEstado_I")
+ # selectDepartamento.clear()
+ # selectDepartamento.send_keys(departamento)
+ 
+ 
+ # #selectCiudad = WebDriverWait(driver, secs).until(find,ctl00_ContentPlaceHolder1_ucOrcamentoOnline_cboCidade_I)
+ 
+ # #selectCiudad = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_cboCidade_I")
+ # selectCiudad.clear()
+ # selectCiudad.send_keys(ciudad)
+ 
+ # driver.implicitly_wait(30)
+ 
+ # selectBarrio = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_cboBairro_I")
+ # selectBarrio.clear()
+ # selectBarrio.send_keys(barrio)
+ 
+ # driver.implicitly_wait(30)
+ 
+ # selectComo = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_cboMidia_I")
+ # selectComo.clear()
+ # selectComo.send_keys("Internet")
+ 
+ # driver.implicitly_wait(15)
+ 
+ # selectDonde = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_cboMidia_I")
+ # selectDonde.clear()
+ # selectDonde.send_keys("Email")
+ 
+ # driver.implicitly_wait(15)
+ 
+ # descripcionField=driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_txtMensagem_I")
+ # descripcionFieldclear()
+ # descripcionField.send_keys(desc)
+ 
+ # driver.implicitly_wait(15)
+ 
+ # submitBtn=driver.find_element_by_id("ctl00_ContentPlaceHolder1_ucOrcamentoOnline_btnEnviar")
+ # driver.get_screenshot_as_file("capturaDoctorAntesEnvio.png") 
+ # submitBtn.click()
+
+ # print("-------------")
+ # print("-------------")
+
+
+ # #lucky_button.click()
+ # print(driver.title)
+ # # capture the screen
+ # driver.get_screenshot_as_file("capturaDoctorDespuesEnvio.png") 
+ 
+ # def find(driver,id):
+    # element = driver.find_elements_by_id(id)
+    # if element:
+        # return element
+    # else:
+        # return False
 electricistas24Horas("Ricardo","3000000000","t.kavanagh@uniandes.edu.co","Necesito que arrglen a mi perro aaaaaaaaaaaaaaaaaaaaaa xp")
 expertoYa("Ricardo","3000000000","t.kavanagh@uniandes.edu.co","necesito un cerrajero ","cll 34sur#51-22",1,"9 Mayo 2018","20")
+#doctorSolucion("Ricardo","3142352865","305717646","jularenas@gmail.com","envio de prueba","Bogotá","Bogotá","BOCHICA")
